118b65cfea1f865de9a313091930c56931898f60
SAGA dev <sagarrai9547@gmail.com>
2026-03-22 23:28:31 +0530

    Add training pipeline constants and configuration paths

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -16,6 +16,13 @@ FILE_NAME: str= "phisingData.csv"
 TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME: str = "test.csv"
 
+"""
+SCHEMA FILE PATH 
+(to validate/check the schema of a data for data_validaton, the data which comes after ingestion)
+"""
+
+SCHEMA_FILE_PATH = os.path.join("data_schema", "schema.yaml")
+
 
 """
 Data Ingestion related constant starts with DATA_INGESTION VAR NAME
@@ -27,3 +34,13 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
+
+"""
+Data Validation related constant starts with DATA_VALIDATION VAR NAME
+"""
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_VALID_DIR: str = "validated"
+DATA_VALIDATION_INVALID_DIR: str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
