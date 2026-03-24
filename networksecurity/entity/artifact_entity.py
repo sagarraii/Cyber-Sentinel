30baad25d5bb91a8b72dd354d308ef9e00a60962
SAGA dev <sagarrai9547@gmail.com>
2026-03-24 22:27:23 +0530

    Create artifact entities to structure pipeline outputs

--- networksecurity/entity/artifact_entity.py
+++ networksecurity/entity/artifact_entity.py
@@ -13,3 +13,21 @@ class DataValidationArtifact:
     valid_test_file_path: str
     invalid_test_file_path: str
     drift_report_file_path: str
+
+@dataclass
+class DataTransformationArtifact:
+    tranformed_object_file_path: str
+    tranformed_train_file_path: str
+    tranformed_test_file_path: str
+
+@dataclass
+class ClassificationMetricArtifact:
+    f1_score: float
+    precision_score: float
+    recall_score: float
+
+@dataclass
+class ModelTrainerArtifact:
+    trained_model_file_path: str
+    train_metric_artifact: ClassificationMetricArtifact
+    test_metric_artifact: ClassificationMetricArtifact
