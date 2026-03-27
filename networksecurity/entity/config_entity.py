7b8ae435dfa7ae75071d28af317a87e1db30ce98
SAGA dev <sagarrai9547@gmail.com>
2026-03-27 13:04:46 +0530

    Add configuration entity classes for pipeline components

--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -16,6 +16,7 @@ class TrainingPipelineConfig:
         self.artifact_name = training_pipeline.ARTIFACT_DIR
 
         self.artifact_dir = os.path.join(self.artifact_name, timestamp)
+        self.model_dir = os.path.join("final_model")
 
         self.timestamp: str = timestamp
 
